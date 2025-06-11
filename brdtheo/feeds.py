7d14425d3368199da55b08d5b2ebe8931b2b6971
Theo Billardey <toto.billardey@gmail.com>
2025-06-11 16:03:15 +0200

    feat(feeds): create RSS feed for posts

---
+++ brdtheo/feeds.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from django.contrib.syndication.views import Feed
+from django.db.models import QuerySet
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse
+
+from blog.models import Post
+
+
+class RssPostFeeds(Feed):
+    title = "Théo Billardey - Blog"
+    link = "/blog/"
+    description = "Latest blog posts from brdtheo.com."
+
+    def items(self) -> QuerySet[Post, Post]:
+        return Post.objects.order_by("-created_at")[:100]
+
+    def item_title(self, item: Post) -> str:
+        return item.title
+
+    def item_description(self, item: Post) -> str:
+        return truncatewords(item.content, 100)
+
+    def item_lastupdated(self, item: Post) -> datetime | None:
+        return item.updated_at
+
+    def item_link(self, item: Post) -> str:
+        return reverse("blog-post", args=[item.slug])
