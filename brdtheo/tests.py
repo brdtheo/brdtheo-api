4e1a416587d24196de64078fb3683fead4982244
Theo Billardey <toto.billardey@gmail.com>
2025-06-14 14:12:14 +0200

    feat(views): add robots.txt
    Create base url for robots.txt file. Prevent crawling of subdomain

---
+++ brdtheo/tests.py
@@ -0,0 +1,12 @@
+from http import HTTPStatus
+
+from django.test import TestCase
+
+
+class RobotsTxtTestCase(TestCase):
+    def test_get(self):
+        response = self.client.get("/robots.txt")
+
+        assert response.status_code == HTTPStatus.OK
+        assert response["content-type"] == "text/plain"
+        assert response.content.startswith(b"User-Agent: *\n")
