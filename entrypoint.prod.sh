822c17fafba0ba251b054ca9b0168d04cebae0a9
Theo <toto.billardey@gmail.com>
2025-09-26 18:24:08 +0200

    build(entrypoint): use uv for entrypoint script

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-python -m gunicorn --bind 0.0.0.0:8004 --workers 3 brdtheo.wsgi:application
+uv run manage.py collectstatic --noinput
+uv run manage.py migrate --noinput
+uv run -m gunicorn --bind 0.0.0.0:8004 --workers 3 brdtheo.wsgi:application
