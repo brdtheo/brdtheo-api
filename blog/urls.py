d8e072b3fa612a546a69a63b3b5fe0654ed458b7
Theo <toto.billardey@gmail.com>
2025-09-29 16:44:03 +0200

    feat(urls): create default router

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import URLPattern, URLResolver, path
-
-from .views import index, post
-
-urlpatterns: list[URLResolver | URLPattern] = [
-    path("", index, name="blog"),
-    path("<str:slug>", post, name="blog-post"),
-]
