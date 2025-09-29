dfd2502b5d9f1be5a4e66b6582d955eaf8fabb0c
Theo <toto.billardey@gmail.com>
2025-09-29 18:06:00 +0200

    feat(blog): create latest post viewset action

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
 from .models import Post, PostCategory
 from .serializers import PostCategorySerializer, PostSerializer
@@ -12,3 +16,9 @@ class PostCategoryViewSet(viewsets.ReadOnlyModelViewSet):
 class PostViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Post.objects.filter(is_published=True)
     serializer_class = PostSerializer
+
+    @action(detail=False, methods=["get"])
+    def latest(self, request: HttpRequest) -> HttpResponse:
+        latest_post = Post.objects.last()
+        serializer = PostSerializer(latest_post)
+        return Response(serializer.data, status=HTTP_200_OK)
